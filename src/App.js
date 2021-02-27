2a006c154fc30215dd4a400e79cbd16d830f4787
TQ <756342530@qq.com>
2021-02-27 14:30:40 +0800

    创建路由

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import './App.css'
+
+import {Switch, Route} from 'react-router-dom'
+import Footer from './components/Footer'
+import Home from './view/Home'
+import About from './view/About'
+import Header from './components/Header'
+import History from './view/History'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/history' component={History}/>
+        <Route path='/about' component={About}/>
+      </Switch>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
