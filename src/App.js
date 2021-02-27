e55b496dfa4f05fb8d6edf604a4e5535d331fb40
TQ <756342530@qq.com>
2021-02-27 21:45:17 +0800

    实现懒加载

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css'
-
+import React, {Suspense, lazy} from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Footer from './components/Footer'
-import Home from './view/Home'
-import About from './view/About'
 import Header from './components/Header'
-import History from './view/History'
+import Loading from './components/Loading'
+
+
+const Home = lazy(() => import('./view/Home'))
+const About = lazy(() => import('./view/About'))
+const History = lazy(() => import('./view/History'))
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Switch>
-        <Route path='/' exact component={Home}/>
-        <Route path='/history' component={History}/>
-        <Route path='/about' component={About}/>
-      </Switch>
+      <Suspense fallback={<Loading/>}>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/history' component={History}/>
+          <Route path='/about' component={About}/>
+        </Switch>
+      </Suspense>
       <Footer/>
     </div>
   )
